0fd712d6bd118b575bcdbbe171f2f2f97d269c4a
Vincent0Lee <vincent0lee95@gmail.com>
2022-01-10 22:10:12 +0800

    S9 Mis.: Effects, Reducers, Context

--- 4_Dummy_Auth/README.md
+++ 4_Dummy_Auth/README.md
@@ -1,1 +1,61 @@
 # Dummy Auth (Effects, Reducers, Context)
+
+## Effect - The Third Hook
+
+### 1. What are the side effects? Anything Else
+
+Tasks happen outside of the normal component evaluation and render cycle.</br>
+Since they might block, delay rendering.(might occur infinite loop)
+
+- Store Data in Browser Storage
+- Send HTTP request to Backend Servers
+- Set & Manage Timers ....
+
+#### 2. Use Examples
+
+1. **No dependency**: never do that. useEffect() is useless, since the inner function will run every re-render.
+2. **Empty dependency []**: only run once, when the app starts up (like componentDidMount()). App.js
+3. **Normal case [usrId, age]** Add all "things" you use in your effect function if those "things" could change because your component (or some parent component) re-rendered. Login.js
+4. **Bouncing case & cleanup function** See Login.js L48-64 for details.
+
+## Reducer - The Forth Hook
+
+### 1. Background
+
+- When the states are complex (multiple states, multiple ways of changing it). useState() might be hard or error-prone to use.
+- Some states are related (email & isValid)
+- useReducer() more powerful state management (To avoid buggy, inefficient code).
+
+### 2. Use Examples
+
+**Login.js**
+
+- Reducer function could be in a separate file (unrelated with rendering component)
+- Two ways to optimise the useEffect (decrease unnecessary run):</br>
+  a) Debouncing: use setTimeout and cleanup function to reduce the frequent call (input field updates in every key stroke). </br>
+  b) Deconstruction: if use reducer, deconstruct the state and give them new name added to dependency
+
+## Context API
+
+### 1. Background
+
+- props: most cases to configure comp. useContext() if the transfer level is too deep.
+
+### 2. Example
+
+### 3. Limitation
+
+- React Context is not optimized for high frequency changes!
+- Better tool: Redux
+- React Context shouldn't be used to replace all component communication & props. </br>
+  Component should still be configurable via props and short 'props chain' might not need any replacement.
+
+## Rules of Hooks
+
+1. Only call React Hooks in **React Functions**.
+2. Only call React Hooks at the **Top Level**.
+3. ALWAYS add everything you refer to inside of useEffect as a dependency!
+
+## Rules of Hooks
+
+Mystery Stuff: **Input.js** React.forwardRef, useImperativeHandle()
--- 4_Dummy_Auth/src/components/Login/Login.js
+++ 4_Dummy_Auth/src/components/Login/Login.js
@@ -1,78 +1,135 @@
-import React, { useState } from 'react';
+import React, {
+  useState,
+  useReducer,
+  useEffect,
+  useContext,
+  useRef,
+} from 'react';
 
 import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
+import Input from '../UI/Input/Input';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
+import classes from './Login.module.css';
+
+// Could be in a separate file.
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
+  }
+  if (action.type === 'INPUT_BLUR') {
+    // the state could be guaranteed the latest one
+    return { value: state.value, isValid: state.value.includes('@') };
+  }
+  return { value: '', isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === 'INPUT_BLUR') {
+    // the state could be guaranteed the latest one
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: '', isValid: false };
+};
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
+  });
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: '',
+    isValid: null,
+  });
+  const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  // Issue 2 after using useReducer(): it updates too often .value and .isValid
+  // Hence, deconstruction to further optimise useEffect()
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+  // It is not a normal side effect, like http request.
+  // But we are re-render the interface based on some state changes, which is also a side effect.
+  useEffect(() => {
+    // Directly approach is not ideal. It will run every key-stroke!
+    // Hence, we use debouncing to optimise useEffect()
+    const identifier = setTimeout(() => {
+      console.log('Checking for validity!');
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+
+    // Cleanup function: runs before every new effect function execution and before the component is removed.
+    // But it won't rin at the very first execution
+    return () => {
+      console.log('Cleanup!');
+      clearTimeout(identifier);
+    };
+
+    // setFormIsValid can be omitted, since it is default function that never changed
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+    // when run this, this might not have the newest enteredPassword or other.(depends on other previous state)
+    // Like said before, should use the preState (function mode) to set value.
+    // setFormIsValid(event.target.value.includes('@') && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({ type: 'INPUT_BLUR' });
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ type: 'INPUT_BLUR' });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={enteredEmail}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={enteredPassword}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          ref={emailInputRef}
+          id="email"
+          label="E-Mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+          ref={passwordInputRef}
+          id="password"
+          label="Password"
+          type="password"
+          isValid={passwordIsValid}
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
