d83a3e893ec97fd6807bd7421c1d7a6f0c4d81f7
Vincent0Lee <vincent0lee95@gmail.com>
2022-01-11 19:22:32 +0800

    S14 Send HTTP request

--- 5_StarWar_Movies/README.md
+++ 5_StarWar_Movies/README.md
@@ -1,1 +1,11 @@
 # Star War Movies (Sending HTTP requests)
+
+## Fairly Familiar Topic
+
+### Things to pay attention on:
+
+- When sending request in useEffect(), to avoid the infinite loop, wrap the request function in useCallback() </br>
+  **App.js L12-39, 57-59**
+- js object to JSON(in request body): JSON.stringify(); On the contrary: response.json()
+- isLoading & error normally are not globalized state. Using conditional rendering to handle each cases:</br>
+  state && Comp.; Or let abc = default, use if to change the content of abc.
--- 5_StarWar_Movies/src/App.js
+++ 5_StarWar_Movies/src/App.js
@@ -1,32 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const transformedMovie = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(transformedMovie);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    // stop loading no matter success or fail.
+    setIsLoading(false);
+  }, []);
+
+  const addMovieHandler = async (movie) => {
+    const response = await fetch('https://something.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
+  // Function is object, so it will change each time re-render the Comp.
+  // Hence if add it to the dependency, there is a infinite loop.
+  // We could omit it of course, but subtle bugs exist when the function involves external states.
+  // Solution: useCallback();
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = <p>Found No movies...</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = error;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
