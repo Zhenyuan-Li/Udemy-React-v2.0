5ff57d36d6fdd25318d2a86c408ed986ef0231a2
Vincent0Lee <vincent0lee95@gmail.com>
2022-01-13 23:40:00 +0800

    S19 Async Tasks with Redux

--- 9_Shopping_Cart/README.md
+++ 9_Shopping_Cart/README.md
@@ -1,1 +1,13 @@
 # Fancy Counter (Async Tasks with Redux)
+
+## Intuitive
+
+- Reducers must be pure, side-effect free, synchronous functions.
+- Two option: </br>
+
+  1. Inside the components (e.g. useEffect())</br>
+     See Big giant useEffect in **App.js L20-61**. It is a way, but not good for me.
+
+  2. Inside the action creators</br>
+     - Thunk: A function that delays an action until later. An action creator function that does NOT return the action itself but another function which eventually returns the action.
+     - A much better approach in this case. The logic is separate from component, much cleaner code. **App.js L64-68**
--- 9_Shopping_Cart/src/App.js
+++ 9_Shopping_Cart/src/App.js
@@ -1,13 +1,93 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+// import { uiActions } from './store/ui-slice';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const showCard = useSelector((state) => state.ui.cardIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    // // Version 1
+    // const sendCartData = async () => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'pending',
+    //       title: 'Sending...',
+    //       message: 'Sending cart data!',
+    //     })
+    //   );
+    //   const response = await fetch(
+    //     'https://review-react-default-rtdb.firebaseio.com/cart.json',
+    //     {
+    //       method: 'PUT',
+    //       body: JSON.stringify(cart),
+    //     }
+    //   );
+    //   if (!response.ok) {
+    //     throw new Error('Sending cart data failed.');
+    //   }
+    //
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'success',
+    //       title: 'Success!',
+    //       message: 'Sending cart successfully!',
+    //     })
+    //   );
+    // };
+
+    // if (isInitial) {
+    //   isInitial = false;
+    //   return;
+    // }
+    // sendCartData().catch((error) => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'error',
+    //       title: 'Error!',
+    //       message: 'Sending cart failed!',
+    //     })
+    //   );
+    // });
+
+    // Version 2
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCard && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
